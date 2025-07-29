637c24690fe11de4b85d10fe691e52ec53b86782
Dien-Nhung Nguyen-Phu <kein@kienlab.com>
2025-07-29 14:04:10 +0700

    wip: add audio visualization to a mode between normal and download
    needs a way to setup the mode as work queue cuz the current
    implementation is polling inside the button interrupt routine.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "CH58x_sys.h"
 #include "CH58xBLE_LIB.h"
 
+#include "audio.h"
 #include "leddrv.h"
 #include "button.h"
 #include "bmlist.h"
@@ -30,6 +31,7 @@ enum MODES {
 	NORMAL,
 	DOWNLOAD,
 	POWER_OFF,
+	AUDIO,
 	MODES_COUNT,
 };
 
@@ -60,6 +62,7 @@ static void change_brightness()
 
 static void mode_setup_download();
 static void mode_setup_normal();
+static void mode_setup_audio_visualize();
 
 __HIGH_CODE
 static void change_mode()
@@ -69,6 +72,7 @@ static void change_mode()
 		NULL,
 		mode_setup_normal,
 		mode_setup_download,
+		mode_setup_audio_visualize,
 		poweroff
 	};
 
@@ -398,6 +402,29 @@ static void mode_setup_normal()
 	start_normal_animation();
 }
 
+static void audio_visualize_poll()
+{
+	while (1) {
+		for (int i=0; i<LED_COLS; i++) {
+			// FIXME: how to adjust this 300 dynamically
+			fb[i] = amp_wav_lut[(abs(mic_adc()) / 300)];
+		}
+		if (mode =! AUDIO) {
+			break;
+		}
+	}
+}
+
+static void mode_setup_audio_visualize()
+{
+	tmos_stop_task(common_taskid, ANI_NEXT_STEP);
+	tmos_stop_task(common_taskid, ANI_MARQUE);
+	tmos_stop_task(common_taskid, ANI_FLASH);
+	tmos_stop_task(common_taskid, BLE_NEXT_STEP);
+
+	audio_visualize_poll();
+}
+
 void handle_after_rx()
 {
 	if (badge_cfg.reset_rx) {
@@ -406,48 +433,6 @@ void handle_after_rx()
 		mode_setup_normal();
 	}
 }
-
-void mic_init() {
-	/* adc 11 - pa7 */
-	GPIOA_ModeCfg(GPIO_Pin_7, GPIO_ModeIN_Floating);
-	ADC_ExtSingleChSampInit(SampleFreq_3_2, ADC_PGA_0);
-	ADC_ChannelCfg(11);
-}
-
-// TODO: setup an interrupt instead of polling, now is just for POC
-int16_t mic_adc() {
-	ADC_ChannelCfg(11);
-	uint64_t sum = 0;
-	for (int i = 0; i < 64; i++) {
-		sum += ADC_ExcutSingleConver();
-		// mDelayuS(1);
-	}
-
-	// adc idle level is 2048. Normalizing this to 0
-	return sum / 64 - 2048;
-}
-
-uint16_t amp_wav_lut[8] = {
-	0b00000000000,
-	0b00000100000,
-	0b00001110000,
-	0b00011111000,
-	0b00111111100,
-	0b01111111110,
-	0b11111111111,
-	0b11111111111,
-};
-
-uint16_t amp_wav_lut_w1[8] = {
-	0b00000100000,
-	0b00001110000,
-	0b00011111000,
-	0b00111111100,
-	0b01111111110,
-	0b11111111111,
-	0b11111111111,
-	0b11111111111,
-};
 
 int main()
 {
@@ -490,20 +475,6 @@ int main()
 	spawn_tasks();
 	
 	mic_init();
-	while (1) 	{
-		for (int i=0; i<LED_COLS; i++) {
-			// need to align this value to center by div by 340
-			int16_t v = mic_adc();
-			// fb[i] = v > 0 ?
-			// 	(1 << 6) << ((v) / 100) :
-			// 	(1 << 6) >> ((-v) / 100);
-			
-			fb[i] = amp_wav_lut[(abs(v) / 300)]; // FIXME: how to adjust this 300 dynamically
-		}
-		if (mode =! NORMAL) 
-			poweroff();
-		// DelayMs(10);
-	}
 
 	mode = NORMAL;
 	while (1) {
