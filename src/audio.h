fe46ee3b7f1bbf8831195e06f4aa0f201d5106a6
Michael Hummels <m.hummels@microsynetics.de>
2025-12-30 12:41:28 +0100

    fix: move definition of audio lookup tables into c file

--- src/audio.h
+++ src/audio.h
@@ -3,27 +3,8 @@
 
 #include <stdint.h>
 
-const uint16_t amp_wav_lut[8] = {
-	0b00000000000,
-	0b00000100000,
-	0b00001110000,
-	0b00011111000,
-	0b00111111100,
-	0b01111111110,
-	0b11111111111,
-	0b11111111111,
-};
-
-const uint16_t amp_wav_lut_w1[8] = {
-	0b00000100000,
-	0b00001110000,
-	0b00011111000,
-	0b00111111100,
-	0b01111111110,
-	0b11111111111,
-	0b11111111111,
-	0b11111111111,
-};
+extern const uint16_t amp_wav_lut[8];
+extern const uint16_t amp_wav_lut_w1[8];
 
 int16_t mic_adc();
 void mic_init();
