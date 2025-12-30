68992cbf9435520ec8dd8c8b2b7718c6ebbe5211
Michael Hummels <m.hummels@microsynetics.de>
2025-12-30 12:41:28 +0100

    fix: move definition of audio lookup tables into c file

--- src/audio.c
+++ src/audio.c
@@ -1,6 +1,29 @@
 #include "CH58x_common.h"
 
 #include "leddrv.h"
+#include "audio.h"
+
+const uint16_t amp_wav_lut[8] = {
+	0b00000000000,
+	0b00000100000,
+	0b00001110000,
+	0b00011111000,
+	0b00111111100,
+	0b01111111110,
+	0b11111111111,
+	0b11111111111,
+};
+
+const uint16_t amp_wav_lut_w1[8] = {
+	0b00000100000,
+	0b00001110000,
+	0b00011111000,
+	0b00111111100,
+	0b01111111110,
+	0b11111111111,
+	0b11111111111,
+	0b11111111111,
+};
 
 // TODO: setup an interrupt instead of polling, now is just for POC
 int16_t mic_adc()
