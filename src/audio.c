cee972f0e4f47d4721a2dc6f007cfdfda6b6ad2f
Siddarth Pai <siddarthpai.2006@gmail.com>
2026-05-29 17:50:04 +0400

    Switching from finding average to using the peaks as avg method is not working on the badge and its showing noise throughout

--- src/audio.c
+++ src/audio.c
@@ -25,24 +25,27 @@ const uint16_t amp_wav_lut_w1[8] = {
 	0b11111111111,
 };
 
-// TODO: setup an interrupt instead of polling, now is just for POC
+static int16_t mic_baseline = 2048; // calibrated at init
+
 int16_t mic_adc()
 {
-	ADC_ChannelCfg(11);
-	uint64_t sum = 0;
-	for (int i = 0; i < 64; i++) {
-		sum += ADC_ExcutSingleConver();
-		// mDelayuS(1);
-	}
-
-	// adc idle level is 2048. Normalizing this to 0
-	return sum / 64 - 2048;
+    ADC_ChannelCfg(11);
+    int16_t peak = 0;
+    for (int i = 0; i < 64; i++) {
+        int16_t sample = (int16_t)ADC_ExcutSingleConver() - mic_baseline;
+        int16_t abssample = sample < 0 ? -sample : sample;
+        if (abssample > peak) peak = abssample;
+    }
+    return peak;
 }
 
 void mic_init()
 {
-	/* adc 11 - pa7 */
-	GPIOA_ModeCfg(GPIO_Pin_7, GPIO_ModeIN_Floating);
-	ADC_ExtSingleChSampInit(SampleFreq_3_2, ADC_PGA_0);
-	ADC_ChannelCfg(11);
+    GPIOA_ModeCfg(GPIO_Pin_7, GPIO_ModeIN_Floating);
+    ADC_ExtSingleChSampInit(SampleFreq_3_2, ADC_PGA_0);
+    ADC_ChannelCfg(11);
+
+    uint64_t sum = 0;
+    for (int i = 0; i < 64; i++) sum += ADC_ExcutSingleConver();
+    mic_baseline = (int16_t)(sum / 64);
 }
